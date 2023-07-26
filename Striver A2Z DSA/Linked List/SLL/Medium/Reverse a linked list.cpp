33ef6d7c2e3f4f03bc83b81268487db1d0f847c0
Saakshi Kobarne <115942356+Saakshik15@users.noreply.github.com>
2023-07-26 20:51:44 +0530

    Update Reverse a linked list.cpp

--- Striver A2Z DSA/Linked List/SLL/Medium/Reverse a linked list.cpp
+++ Striver A2Z DSA/Linked List/SLL/Medium/Reverse a linked list.cpp
@@ -2,7 +2,7 @@
 Problem statement : Given the head of a singly linked list, reverse the list, and return the reversed list.
 */
 
-//approach : reversing the pointers (iterative approach)
+//approach 1 : reversing the pointers (iterative approach)
 
 //code : (Iterative approach)
 
@@ -17,6 +17,28 @@ ListNode* reverseList(ListNode* head) {
             head= temp;
         }
         return newHead;
+    }
+
+//time complexity : O(N)
+//space complexity : O(1)
+
+/*
+Approach 2 : recursive approach 
+
+We traverse to the end of the list recursively.
+As we reach the end of the list, we make the end node the head. Then receive previous nodes and make them connected to the last one.
+At last, we link the second node to the head and the first node to NULL. We return to our new head.
+*/
+
+//code :
+
+ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* prev = NULL;
+        ListNode* h2 = reverseList(head->next);
+        head->next->next = head;
+        head->next=prev;
+        return h2;
     }
 
 //time complexity : O(N)
