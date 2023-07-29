66e7018a03450f0a1f14020db874ad4a423344c2
Saakshi Kobarne <115942356+Saakshik15@users.noreply.github.com>
2023-07-29 20:05:33 +0530

    Update String to Integer(atoi).cpp

--- Striver A2Z DSA/Recursion/Get Hold/String to Integer(atoi).cpp
+++ Striver A2Z DSA/Recursion/Get Hold/String to Integer(atoi).cpp
@@ -115,3 +115,28 @@ int myAtoi(string s) {
 /*
 Approach 2 : recursive approach
 */
+
+//code : 
+
+long atoi(string s, int sign, int i, long result){
+        if(sign*result >= INT_MAX) return INT_MAX;
+        if(sign*result <= INT_MIN) return INT_MIN;
+
+        if(i>=s.length() || s[i]<'0' || s[i] >'9') return sign*result;
+
+        result= atoi(s, sign, i+1, result*10+s[i]-'0');
+        return result;
+    }
+
+    int myAtoi(string s) {
+        int i=0, n= s.length(), sign= 1;
+
+        while(i<n && s[i]==' ') ++i;
+        if(s[i] == '-')sign= -1, ++i;
+        else if(s[i] == '+') ++i;
+
+        return (int)atoi(s, sign, i, 0);
+    }
+
+//time complexity : O(N)
+//space complexit : O(N)
