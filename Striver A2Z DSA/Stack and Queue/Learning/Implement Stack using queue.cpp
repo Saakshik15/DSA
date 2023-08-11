13556b80c73aa666a1ea3c991074a01769dca54f
Saakshi Kobarne <115942356+Saakshik15@users.noreply.github.com>
2023-08-11 22:15:17 +0530

    Update Implement Stack using queue.cpp

--- Striver A2Z DSA/Stack and Queue/Learning/Implement Stack using queue.cpp
+++ Striver A2Z DSA/Stack and Queue/Learning/Implement Stack using queue.cpp
@@ -53,6 +53,61 @@ top(){
   return Q1.front();
 }
 
+//code (from leetcode) :
+
+class MyStack {
+public:
+    queue<int> q1, q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        while(q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int x = q1.front();
+        q1.pop();
+        swap(q1, q2);
+        return x;
+    }
+    
+    int top() {
+        while(q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int x = q1.front();
+        q2.push(q1.front());
+        q1.pop();
+        swap(q1, q2);
+        return x;
+    }
+    
+    bool empty() {
+        if(q1.empty() and q2.empty()) return true;
+        else return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+
+
+
 /*
 Problem Statement 2:
 Implement a Stack using a single Queue.
@@ -85,6 +140,49 @@ pop(){
 top(){
   Q1.front();
 }
+
+//code from leetcode :
+
+class MyStack {
+public:
+
+    queue<int> q;
+
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i=0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int a=q.front();
+        q.pop();
+        return a;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
 
 //time complexity of push operation : O(N)
 //space complexity : O(N)
