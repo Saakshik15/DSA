d896ffd6de3bb579bbc640d9b009020e63fef80d
Saakshi Kobarne <115942356+Saakshik15@users.noreply.github.com>
2023-09-14 20:48:15 +0530

    Update Sliding window maximum.cpp

--- Striver A2Z DSA/Sliding window & two pointer/Medium/Sliding window maximum.cpp
+++ Striver A2Z DSA/Sliding window & two pointer/Medium/Sliding window maximum.cpp
@@ -1,4 +1,5 @@
 /*
+Problem statement:
 Given an array arr[] of size N and an integer K. Find the maximum for each and every contiguous subarray of size K.
 
 Example 1:
@@ -20,8 +21,49 @@ Explanation:
 /*
 Solution: 
 
-Approach:
+Approach: Sliding window
 
+Create a deque to keep the necessary elements only, eg, as we slide the window if we encouter any element greater than what currently
+is present in the deque, then we will remove that element from it as it is not useful (since there is a greater element in the current
+window than that). Also, the elements after the current max even though smaller than current max shall be kept as when we remove the 
+current max and when it won't be there in the next window, the next element might be the max element for the next window.
+
+For each window, the answer will be always the front element of deque
+
+sliding the window: while sliding the window, if front of deque is the first element of window, we will remove it
+else we will just simply slide the window
 
 */
 
+//code :
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> deq;
+        vector<int> ans;
+        int l=0, r=0;
+
+        while(r < nums.size()){
+
+          //if current element is greater than elements in the queue, pop all elements so that only useful elements will be in deque
+            while(!deq.empty() && nums[r] > deq.back())
+                deq.pop_back();
+            deq.push_back(nums[r]);
+
+          //if window size is obtained
+            if(r-l+1 == k){
+              //if window size is obtained get answer
+                ans.push_back(deq.front());
+
+              //if l is part of the deque, remove it so that we can slide the window
+                if(nums[l] == deq.front())
+                    deq.pop_front();
+                l++;
+            }
+          //writing r++ outside the conditions make sure r is always incremented regardless we obtain the window size or not
+            r++;
+        }
+        return ans;
+    }
+
+//time complexity : O(N)
+//space complexity : O(K)
