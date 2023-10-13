ed5f8ab27d5cfd7a6b987d32109951916af58903
Saakshi Kobarne <115942356+Saakshik15@users.noreply.github.com>
2023-10-13 20:34:53 +0530

    Create Count nomber of nice subarrays.cpp

---
+++ Striver A2Z DSA/Sliding window & two pointer/Medium/Count nomber of nice subarrays.cpp
@@ -0,0 +1,44 @@
+/*
+Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+Return the number of nice sub-arrays.
+
+Example 1:
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+
+Example 2:
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There is no odd numbers in the array.
+
+Example 3:
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+*/
+
+// code:
+
+class Solution {
+public:
+
+    int subarrays(vector<int>& nums, int k){
+        int odd=0, ans=0, i=0, j=0;
+
+        while(j<nums.size()){
+            if(nums[j]%2!=0) odd++;
+
+            while(odd>k){
+                if(nums[i]%2!=0) odd--;
+                i++;
+            }
+            ans+= j-i+1;
+            j++;
+        }
+        return ans;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return subarrays(nums,k)- subarrays(nums,k-1);
+    }
+};
