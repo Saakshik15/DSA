95ebda5c1e231dd435bc446d3977ce435d084d7a
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2023-10-19 13:35:38 +0530

    Update Kth largest element in an array(using priority queue).cpp

--- Striver A2Z DSA/Heaps/Medium/Kth largest element in an array(using priority queue).cpp
+++ Striver A2Z DSA/Heaps/Medium/Kth largest element in an array(using priority queue).cpp
@@ -3,7 +3,17 @@ Given an integer array nums and an integer k, return the kth largest element in 
 Note that it is the kth largest element in the sorted order, not the kth distinct element.
 */
 
-//code 1:
+//using min heap
+
+/*
+In min heap we will always be storing k elements.
+We will then traverse the array and add first k elements to the min heap.
+Then, for the next element, when min heap is full with k elements, then for inserting the next element:
+if the next element is greater than top of the min heap we will pop the top element and insert the next element.
+If the next element is smaller than the top then the element is not a candidate of largest elements so we will not push it on min heap.
+*/
+
+//code :
 
 class Solution {
 public:
@@ -11,34 +21,24 @@ public:
         priority_queue<int, vector<int>, greater<int>> 
         min_heap(nums.begin(), nums.begin() + k);
         
-        for (int i = k; i < nums.size(); i++) {
-            if (nums[i] > min_heap.top()) {
+        for (auto i: nums) {
+            if (i > min_heap.top()) {
                 min_heap.pop();
-                min_heap.push(nums[i]);
+                min_heap.push(i);
             }
         }
-        
         return min_heap.top();
     }
 };
+
+/*
+Other approach where instead of having a min heap with size k, if size of min heap exceeds k
+we will pop the top elements till the size of min heap is k.
+So, the top elements which are smaller will be removed from the min heap and 
+as there are exactly k elements in the min heap, then the top element will be the kth largest element of the array.
+*/
 
 //code 2:
-
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> 
-        min_heap(nums.begin(), nums.begin() + k);
-        
-        for (int i = k; i < nums.size(); i++) {
-            min_heap.push(nums[i]);
-            min_heap.pop();
-        }
-        return min_heap.top();
-    }
-};
-
-//code 3:
 
 class Solution {
 public:
@@ -48,8 +48,10 @@ public:
       
         for(int x: nums){
             pq.push(x);
-            if( pq.size()>k) pq.pop();
+            if( pq.size() > k) pq.pop();
         }
         return pq.top();
     }
 };
+
+
