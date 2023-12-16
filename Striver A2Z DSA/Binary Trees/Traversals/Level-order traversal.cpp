5e77ed633493fce4f891195db9dbfb700e269c53
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2023-12-16 20:17:43 +0530

    Create Level-order traversal.cpp

---
+++ Striver A2Z DSA/Binary Trees/Traversals/Level-order traversal.cpp
@@ -0,0 +1,39 @@
+// Level-order traversal: traverse the tree level by level
+
+//code:
+
+// TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+// Level-order traversal
+vector<int> levelOrder(TreeNode<int> * root){    
+    vector<int> ans;
+    queue<TreeNode<int>*> q;
+
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode<int>* currentNode = q.front();
+        q.pop();
+        ans.push_back(currentNode->data);
+        if(currentNode->left) q.push(currentNode->left);
+        if(currentNode->right) q.push(currentNode->right);
+    }
+    return ans;
+}
+
+
+
