d8b49fb9257b5d7fd46fa68e21104e8b4b7e5343
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2023-12-16 20:27:49 +0530

    Create Iterative in-order traversal.cpp

---
+++ Striver A2Z DSA/Binary Trees/Traversals/Iterative in-order traversal.cpp
@@ -0,0 +1,36 @@
+//code:
+
+// Binary Tree Node structure
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+
+//In-order traversal using iterative approach (stack)
+vector<int> getInOrderTraversal(TreeNode *root){
+    vector<int> answer;
+
+    if (root == NULL) return answer;
+
+    stack<TreeNode *> s;
+    TreeNode *current = root;
+
+    while (current != NULL || !s.empty()){
+        while (current != NULL){
+            s.push(current);
+            current = current->left;
+        }
+        current = s.top();
+        s.pop();
+
+        answer.push_back(current->data);
+        current = current->right;
+    }
+    
+    return answer;
+}
