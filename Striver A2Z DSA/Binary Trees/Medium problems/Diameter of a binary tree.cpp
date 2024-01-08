361d01d03f30f1efa314a5f5ec5230aa230ba10c
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-08 14:35:18 +0530

    Update Diameter of a binary tree.cpp

--- Striver A2Z DSA/Binary Trees/Medium problems/Diameter of a binary tree.cpp
+++ Striver A2Z DSA/Binary Trees/Medium problems/Diameter of a binary tree.cpp
@@ -24,5 +24,27 @@ findMax(root){
 
 //optimized approach:
 
+class Solution {
+public:
 
+    int height(TreeNode* node, int& d){
+        if(node == NULL) return 0;
+
+        int lh= height(node->left, d);
+        int rh= height(node->right, d);
+
+        d= max(d, lh+rh);
+
+        return 1+ max(lh, rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int d=0;
+        height(root,d);
+        return d;
+    }
+};
+
+
+// TC: O(N)
 
