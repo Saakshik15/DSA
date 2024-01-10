d708a35ec3192d5bc4fb40f914c655fd74a7ed37
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-10 11:11:36 +0530

    Update Binary tree zigzag level order traversal.cpp

--- Striver A2Z DSA/Binary Trees/Medium problems/Binary tree zigzag level order traversal.cpp
+++ Striver A2Z DSA/Binary Trees/Medium problems/Binary tree zigzag level order traversal.cpp
@@ -15,3 +15,48 @@ we will store the ans in vector<vector<int>>, in the inside vectors we will stor
 
 //code:
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;  //for storing final result
+        if(!root) return res;
+
+        queue<TreeNode*> q;  //queue for traversal
+        q.push(root);
+        bool flag= true; //true-> left to right
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>level(size);   //vector of size of queue for storing each level traversal
+
+            for(int i=0; i<size; i++){
+                TreeNode* node= q.front();  //taking front element of the queue
+                q.pop();
+
+                //if flag is left to right (true) insert as the order is 
+                //if flag is right to left(false) insert in reverse order
+                int idx= (flag)? i: (size-1-i);   
+                level[idx] = node->val;
+
+                //pushing the child nodes of the current node in te queue
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            flag= !flag;            //inverting the flag for the next level traversal
+            res.push_back(level);   //pushing the current level on the result vector
+        }
+        return res;
+    }
+};
