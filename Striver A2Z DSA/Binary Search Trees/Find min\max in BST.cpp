89c4b94c7759fb8f6a3f3e7800cc081d0f8bcf96
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-02-11 19:46:48 +0530

    Update Find min\max in BST.cpp

--- Striver A2Z DSA/Binary Search Trees/Find min\max in BST.cpp
+++ Striver A2Z DSA/Binary Search Trees/Find min\max in BST.cpp
@@ -1,8 +1,17 @@
-//code:
+//code for min
 
 int minVal(Node* root){
 	if(!root) return -1;
 
 	while(root->left) root= root->left;
+	return root->data;
+}
+
+//code for max:
+
+int minVal(Node* root){
+	if(!root) return -1;
+
+	while(root->right) root= root->right;
 	return root->data;
 }
