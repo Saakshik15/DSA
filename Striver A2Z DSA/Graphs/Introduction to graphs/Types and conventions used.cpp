d6d43405a051056ebfa30ab8564186214312bb31
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-02-16 18:27:20 +0530

    Create Types and conventions used.cpp

---
+++ Striver A2Z DSA/Graphs/Introduction to graphs/Types and conventions used.cpp
@@ -0,0 +1,37 @@
+/*
+undirected graph:
+An undirected graph is a type of graph where the edges have no specified direction assigned to the them.
+
+Directed graph:
+A directed graph is defined as a type of graph where the edges have a direction associated with them.
+*/
+
+
+/*
+Cycle in a graph:
+a path that starts from a given vertex and ends at the same vertex is called a cycle.
+There can be both cycles in a directed graph or cycle in an undirected graph.
+
+Path:
+a path in a graph is a finite or infinite sequence of edges which joins a sequence of vertices which, 
+are all distinct (and since the vertices are distinct, so are the edges).
+*/
+
+/*
+Degree of vertex in a graph:
+(For undirected graph)
+the degree of a vertex is the number of edges that are incident to the vertex. 
+It can also be thought of as the number of neighbors a vertex has. 
+
+Total degree of an undirected graph= no of Edges x 2
+
+Degree of a vertex in a drected graph:
+Indegree: Indegree of a vertex is defined as the number of incoming edges incident on a vertex in a directed graph.
+Outdegree: Outdegree of a vertex is defined as the number of outgoing edges from a vertex in a directed graph.
+*/
+
+/*
+Edge weight:
+If edges in a graph are assigned weights, these are edge weights.
+If a particular edge does not have a weight, the weight is assumed to be 1 (unit weight).
+*/
