752cc3b0acdef941f659aeb0ad9a99d59d1ea6eb
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-02-18 18:51:33 +0530

    Update BFS.cpp

--- Striver A2Z DSA/Graphs/Introduction to graphs/BFS.cpp
+++ Striver A2Z DSA/Graphs/Introduction to graphs/BFS.cpp
@@ -1,11 +1,11 @@
 vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         vector<int> bfs;
         
-        int vis[V] ={0};
+        int vis[V] ={0};  //to keep track of visited nodes
         vis[0]=1;
         
         queue<int> q;
-        q.push(0);
+        q.push(0);   //ps has 0 based indexing
         
         while(!q.empty()){
             int node= q.front();
@@ -13,7 +13,7 @@ vector<int> bfsOfGraph(int V, vector<int> adj[]) {
             
             bfs.push_back(node);
             
-            for(auto it: adj[node]){
+            for(auto it: adj[node]){  //for each node, iterate over its adj list which is array
                 if(!vis[it]){
                     q.push(it);
                     vis[it]=true;
@@ -22,3 +22,7 @@ vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         }
         return bfs;
     }
+
+
+
+//
