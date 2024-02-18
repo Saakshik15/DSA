db8549607edef02daef37c7545fe6276a27c0191
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-02-18 18:49:50 +0530

    Create BFS.cpp

---
+++ Striver A2Z DSA/Graphs/Introduction to graphs/BFS.cpp
@@ -0,0 +1,24 @@
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> bfs;
+        
+        int vis[V] ={0};
+        vis[0]=1;
+        
+        queue<int> q;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            
+            bfs.push_back(node);
+            
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    q.push(it);
+                    vis[it]=true;
+                }
+            }
+        }
+        return bfs;
+    }
