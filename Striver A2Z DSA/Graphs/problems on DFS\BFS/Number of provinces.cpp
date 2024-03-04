c5fb7c3da49cba3e4a8aeafd1da0276ec8f45dfa
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-03-04 12:36:28 +0530

    Update Number of provinces.cpp

--- Striver A2Z DSA/Graphs/problems on DFS\BFS/Number of provinces.cpp
+++ Striver A2Z DSA/Graphs/problems on DFS\BFS/Number of provinces.cpp
@@ -5,6 +5,8 @@ However, if the components are not connected, we need to make separate calls for
 So, whenever we will make a call like that, we will add it to the number of provinces.
   */
 
+//by using the adj matrix itself
+
 class Solution {
 public:
 
@@ -28,6 +30,48 @@ public:
                 traverse(isConnected,n,vis,i);
             }
         }
+        return provinces;
+    }
+};
+
+//by converting the adj matrix to adj list and then finding provinces:
+
+class Solution {
+
+private:
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for (auto& adjNode : adj[node]) {
+            if (!visited[adjNode])
+                dfs(adjNode, adj, visited);
+        }
+    }
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+
+        // create adjacency list
+        vector<vector<int>> adj(n, vector<int>());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && isConnected[i][j] == 1)
+                    adj[i].emplace_back(j);
+            }
+        }
+
+     //finding provinces
+        vector<bool> visited(n, false);
+        int provinces = 0;
+
+        for (int node = 0; node < n; node++) {
+            if (!visited[node]) {
+                provinces++;
+                dfs(node, adj, visited);
+            }
+        }
+
         return provinces;
     }
 };
