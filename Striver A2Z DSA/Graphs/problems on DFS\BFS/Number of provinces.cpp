56e2df37b99b94dcf1b57f2500429eb574126e62
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-03-04 12:29:45 +0530

    Update Number of provinces.cpp

--- Striver A2Z DSA/Graphs/problems on DFS\BFS/Number of provinces.cpp
+++ Striver A2Z DSA/Graphs/problems on DFS\BFS/Number of provinces.cpp
@@ -1,1 +1,33 @@
-h
+/*
+If the components are connected, then we dont need to make a separate call, 
+the dfs function will mark the visited nodes as 1 which are connected.
+However, if the components are not connected, we need to make separate calls for each node checking where vis[node] = false.
+So, whenever we will make a call like that, we will add it to the number of provinces.
+  */
+
+class Solution {
+public:
+
+    void traverse(vector<vector<int>>& isConnected, int n, vector<int>& vis, int i){
+        vis[i]=1;
+
+        for(int j=0; j<n; j++){
+            if(isConnected[i][j] && vis[j]==0)
+                traverse(isConnected,n,vis,j);
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n= isConnected.size();
+        vector<int> vis(n,0);
+        int provinces=0;
+
+        for(int i=0; i<n; i++){
+            if(vis[i]==0){
+                provinces+= 1;
+                traverse(isConnected,n,vis,i);
+            }
+        }
+        return provinces;
+    }
+};
